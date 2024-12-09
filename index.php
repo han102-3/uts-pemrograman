88f720ec359acf9be9f932f00ee8de36203adba6
han102-3 <tredingku03@gmail.com>
2024-12-09 22:16:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+// File: input.php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Input Data</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+    <h2>Tambah Member Baru</h2>
+    <form action="proses_input.php" method="POST">
+        <label for="nama">Nama Member:</label><br>
+        <input type="text" id="nama" name="nama_member" required><br><br>
+
+        <label for="level">Level:</label><br>
+        <select id="level" name="level" required>
+            <option value="Gold">Silver</option>
+            <option value="Silver">Gold</option>
+            <option value="Platinum">Platinum</option>
+        </select><br><br>
+
+        <label for="diskon">Diskon Member (%):</label><br>
+        <input type="number" id="diskon" name="diskon_member" required><br><br>
+
+        <button type="submit">Submit</button>
+    </form>
+    <br>
+    <a href="view.php">Lihat Semua Data</a>
+   
+</body>
+<a href="view_report.php">Lihat Keseluruhan Transaksi</a>
+</html>
